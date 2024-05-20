b78d87c728faabf932f483a8995f54313377df13
Anh Phung (Aiden) /Sotatek/OnC Java Developer <e.anh.phung@wesang.com>
2024-05-20 09:48:55 +0700

    update audit

--- order/src/main/java/org/service/order/web/controller/OrderController.java
+++ order/src/main/java/org/service/order/web/controller/OrderController.java
@@ -1,6 +1,5 @@
 package org.service.order.web.controller;
 
-import com.data.entity.OrderEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.service.order.common.SnackRequest;
 import org.service.order.domain.convert.OrderCommandConverter;
@@ -10,7 +9,6 @@ import org.service.order.web.response.OrderResponse;
 import org.service.support.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
