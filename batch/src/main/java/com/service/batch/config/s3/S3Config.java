f4bf649f2f7d8d99e657208cec81cceb6485b2c3
nam.nguyen <nam.nguyen17@sotatek.com>
2024-09-02 01:26:10 +0700

    Config aws connect on aws-s3

--- batch/src/main/java/com/service/batch/config/s3/S3Config.java
+++ batch/src/main/java/com/service/batch/config/s3/S3Config.java
@@ -1,15 +1,11 @@
 package com.service.batch.config.s3;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
